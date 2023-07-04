913137c728781bb3726d72cb17ae8930af3582d0
John Merz <jmerz826@gmail.com>
2023-07-04 15:04:21 -0700

    lint

--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 import { Row } from "./Row";
 import { useState } from "react";
 import { PiecesTray } from "./PiecesTray";
-import { IPiece, Piece } from "./Piece";
-import { pieceMap } from "./pieceMap";
+import { IPiece } from "./Piece";
+import { generateIterations } from "./pieceMap";
 
 const StyledBoard = styled.div`
   height: 400px;
@@ -14,29 +14,6 @@ const StyledBoard = styled.div`
   border: 5px solid gold;
   border-radius: 4px;
 `;
-
-const keys = Object.keys(pieceMap);
-const values = Object.values(pieceMap);
-
-function generateIterations<T extends Record<keyof IPiece, string>>(
-  index = 0,
-  iteration: T = {} as T
-): T[] {
-  if (index === keys.length) {
-    return [iteration];
-  }
-
-  const key = keys[index];
-  const currentValues = values[index];
-  const iterations: T[] = [];
-
-  for (const value of currentValues) {
-    const newIteration = { ...iteration, [key]: value };
-    iterations.push(...generateIterations(index + 1, newIteration));
-  }
-
-  return iterations;
-}
 
 const Board = () => {
   const [availablePieces, setAvailablePieces] = useState<IPiece[]>(
--- src/components/game/pieceMap.ts
+++ src/components/game/pieceMap.ts
@@ -7,4 +7,27 @@ const pieceMap: { [P in keyof IPiece]: IPiece[P][] } = {
   pattern: ["solid", "striped"],
 };
 
-export { pieceMap };
+const keys = Object.keys(pieceMap);
+const values = Object.values(pieceMap);
+
+function generateIterations<T extends Record<keyof IPiece, string>>(
+  index = 0,
+  iteration: T = {} as T
+): T[] {
+  if (index === keys.length) {
+    return [iteration];
+  }
+
+  const key = keys[index];
+  const currentValues = values[index];
+  const iterations: T[] = [];
+
+  for (const value of currentValues) {
+    const newIteration = { ...iteration, [key]: value };
+    iterations.push(...generateIterations(index + 1, newIteration));
+  }
+
+  return iterations;
+}
+
+export { generateIterations };
