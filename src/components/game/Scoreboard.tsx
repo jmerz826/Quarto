ddd54a64e34ce88932999310e45a01f47e651884
John Merz <jmerz826@gmail.com>
2023-08-24 18:53:46 -0700

    scoreboard styling

--- src/components/game/Scoreboard.tsx
+++ src/components/game/Scoreboard.tsx
@@ -3,13 +3,16 @@ import styled from "styled-components";
 import { GameContext, IGameContext } from "./Board";
 
 const StyledScoreboard = styled.div`
-  background-color: beige;
+  background-color: black;
+  color: white;
   height: 50px;
-  margin: 0 20%;
+  margin: 0 25%;
   text-align: center;
   display: flex;
   justify-content: center;
   align-items: center;
+  border-radius: 8px;
+  border: 2px solid gold;
 `;
 
 const Scoreboard = () => {
