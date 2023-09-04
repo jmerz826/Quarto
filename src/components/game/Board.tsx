1c90f3655d382b850a57c8731f3e35ccf09fddee
John Merz <jmerz826@gmail.com>
2023-09-03 18:07:04 -0700

    resp

--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -12,7 +12,6 @@ const StyledBoard = styled.div`
   aspect-ratio: 1 / 1;
   background-color: black;
   /* max-width: 500px; */
-  max-height: 75vh;
   margin: auto;
   margin-top: 50px;
   border: 5px solid gold;
